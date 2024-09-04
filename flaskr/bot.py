76adc568ecff4a32ac995db86bab9731dd2397da
v8phan <56612604+v8phan@users.noreply.github.com>
2024-09-04 14:19:36 -0500

    char list to bot.py

--- flaskr/bot.py
+++ flaskr/bot.py
@@ -4,6 +4,7 @@ import discord
 from readtable import lookupMove, lookupMoveList, findHitbox, findCharPic
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 intents = discord.Intents.default()
@@ -11,27 +12,50 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+characterList = [
+    "Anakaris",
+    "Aulbath",
+    "Bishamon",
+    "Bulleta",
+    "Demitri",
+    "Felicia",
+    "Gallon",
+    "Jedah",
+    "Lei-Lei",
+    "Lilith",
+    "Morrigan",
+    "Q-Bee",
+    "Sasquatch",
+    "Victor",
+    "Zabel",
+]
+
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {client.user}")
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    
-    if message.content.startswith('VSAVbot'):
+
+    if message.content.startswith("VSAVbot"):
         messagesplit = message.content.split()
         inputChara = messagesplit[1]
         inputMove = messagesplit[2]
 
         embed = discord.Embed(
-            colour=discord.Colour.dark_green(),
-            description=inputMove,
-            title=inputChara
+            colour=discord.Colour.dark_green(), description=inputMove, title=inputChara
         )
-        if inputMove == 'movelist':
+
+        if inputChara not in characterList:
+            await message.channel.send(
+                "Character does not exist. First letter of character name is capitalized. Use 'movelist' to return a character's movelist (VSAVbot Victor movelist)."
+            )
+
+        if inputMove == "movelist":
             embed.set_thumbnail(url=findCharPic(inputChara))
             displaymovelist = lookupMoveList(inputChara)
             embed.add_field(name="movelist", value=displaymovelist)
@@ -43,12 +67,17 @@ async def on_message(message):
             embed.add_field(name="", value=str(displaymoves))
             displayHitboxes = findHitbox(inputChara, inputMove)
             for x in displayHitboxes:
-                #print(x)
+                # print(x)
                 embed.set_image(url=x)
             await message.channel.send(embed=embed)
 
-    if message.content.startswith('Ping page'):
-        await message.channel.send(requests.get('https://wiki.gbl.gg/index.php?title=Vampire_Savior/Aulbath/Data&action=edit').status_code)
-        
-DC_TOKEN = os.getenv('DC_TOKEN')
+    if message.content.startswith("Ping page"):
+        await message.channel.send(
+            requests.get(
+                "https://wiki.gbl.gg/index.php?title=Vampire_Savior/Aulbath/Data&action=edit"
+            ).status_code
+        )
+
+
+DC_TOKEN = os.getenv("DC_TOKEN")
 client.run(DC_TOKEN)

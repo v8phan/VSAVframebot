6e9cbd007f3729941f9434f296c81863ea4684e8
v8phan <56612604+v8phan@users.noreply.github.com>
2024-10-12 14:30:45 -0500

    commands no longer case sens

--- flaskr/bot.py
+++ flaskr/bot.py
@@ -44,13 +44,13 @@ async def on_message(message):
     if message.content.startswith("VSAVbot"):
         try:
             messagesplit = message.content.split()
-            inputChara = messagesplit[1]
-            inputMove = messagesplit[2]
+
+            inputChara = messagesplit[1].capitalize()
+            inputMove = messagesplit[2].upper()
         except:
             await message.channel.send(
                 "Please enter a character and a move ('VSAVbot Victor 5LP'). To request the movelist enter character and 'movelist' ('VSAVbot Victor movelist')"
             )
-
 
         embed = discord.Embed(
             colour=discord.Colour.dark_green(), description=inputMove, title=inputChara
@@ -58,10 +58,53 @@ async def on_message(message):
 
         if inputChara not in characterList:
             await message.channel.send(
-                "Character does not exist. First letter of character name is capitalized. Use 'movelist' to return a character's movelist (VSAVbot Victor movelist)."
+                "Character does not exist. Use 'movelist' to return a character's movelist (VSAVbot Victor movelist)."
             )
 
-        if inputMove == "movelist":
+        if inputMove == "MOVELIST":
+            embed.set_thumbnail(url=findCharPic(inputChara))
+            displaymovelist = lookupMoveList(inputChara)
+            embed.add_field(name="movelist", value=displaymovelist)
+            await message.channel.send(embed=embed)
+
+        else:
+            embed.set_thumbnail(url=findCharPic(inputChara))
+            displaymoves = lookupMove(inputChara, inputMove)
+            embed.add_field(name="", value=str(displaymoves))
+            displayHitboxes = findHitbox(inputChara, inputMove)
+            try:
+                for x in displayHitboxes:
+                    # print(x)
+                    embed.set_image(url=x)
+                    # for some reason sending embed here will send all of the hitbox pics if there are multiple but in separate embeds
+                    # await message.channel.send(embed=embed)
+            except:
+                await message.channel.send(
+                    "Move does not exist. First letter of character name is capitalized. Use 'movelist' to return a character's movelist (VSAVbot Victor movelist)."
+                )
+            await message.channel.send(embed=embed)
+
+    if message.content.startswith("vsavbot"):
+        try:
+            messagesplit = message.content.split()
+
+            inputChara = messagesplit[1].capitalize()
+            inputMove = messagesplit[2].upper()
+        except:
+            await message.channel.send(
+                "Please enter a character and a move ('VSAVbot Victor 5LP'). To request the movelist enter character and 'movelist' ('VSAVbot Victor movelist')"
+            )
+
+        embed = discord.Embed(
+            colour=discord.Colour.dark_green(), description=inputMove, title=inputChara
+        )
+
+        if inputChara not in characterList:
+            await message.channel.send(
+                "Character does not exist. Use 'movelist' to return a character's movelist (VSAVbot Victor movelist)."
+            )
+
+        if inputMove == "MOVELIST":
             embed.set_thumbnail(url=findCharPic(inputChara))
             displaymovelist = lookupMoveList(inputChara)
             embed.add_field(name="movelist", value=displaymovelist)
